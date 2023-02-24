05ce848304f5a958859f5f97dae35b5436532dd7
Matias Bensan Araya <mbensan@gmail.com>
2023-02-23 22:43:09 -0300

    Ejemplos de Javascript con la tarea de Bootstrap

---
+++ bootstrap/mi_script.js
@@ -0,0 +1,44 @@
+// variables que almacenan Strings (texto)
+const nombres = 'Matias José'
+
+const apellidos = 'Bensan Araya'
+
+// Una variable a partir de otras variables
+const saludo = 'Hola ' + nombres + ' ' + apellidos
+
+// variables Booleanas (true o false)
+const estaLloviendo = 3 > 8 - 4 ** 2
+// = true
+
+const verdad = 5 == 2 + 3
+// true
+
+const algo = 7 <= 8-2
+// false
+
+
+console.log(saludo)
+
+// variable que almacena números
+const edad = 40
+
+// control de flujo
+if (edad >= 80) {
+  console.log("El usuario tiene " + edad + ' años')
+  console.log("Está muy viejito para beber")
+}
+else if (edad == 40) {
+  console.log("Felicidades por la 4 decadas")
+}
+else if (edad >= 18) {
+  console.log("El usuario tiene " + edad + ' años')
+  console.log("Puedes beber alcohol")
+}
+else {
+  console.log("El usuario recién tiene " + edad + ' años')
+  console.log("NO puede beber alcohol")
+}
+
+// Ciclos
+
+
